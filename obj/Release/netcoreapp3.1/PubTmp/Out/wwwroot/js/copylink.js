f8c09ab194b8f36ca7c6c94171876f97d091e2fa
chuksel95@gmail.com <chuksel95@gmail.com>
2021-11-10 09:48:07 +0100

    newcommit

---
+++ obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/copylink.js
@@ -0,0 +1,8 @@
+﻿function myfunction() {
+    var copyText = document.getElementById("myinput")
+
+    copyText.select();
+    copyText.setselectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copied link :" + copyText.value);
+}
