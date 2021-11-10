cc61549d5065bf0031e7e6fd96f39068c20250f6
chuksel95@gmail.com <chuksel95@gmail.com>
2021-11-10 05:31:53 +0100

    envcommit

--- Startup.cs
+++ Startup.cs
@@ -70,14 +70,22 @@ namespace CeeveeSoftWebProj
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
-            
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
-          
-                //app.UseExceptionHandler("/Home/Error");
+            }
+
+            if (env.IsProduction() || env.IsStaging())
+            {
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-               // app.UseHsts();
+                app.UseHsts();
+            }
+            
+               
+          
+                
             
             app.UseHttpsRedirection();
             app.UseStaticFiles();
