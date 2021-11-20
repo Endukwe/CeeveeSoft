3c507aeb36d199d13253909ac4e0acf6966951b5
chuksel95@gmail.com <chuksel95@gmail.com>
2021-11-20 07:11:55 +0100

    FEupdates

--- wwwroot/js/copylink.js
+++ wwwroot/js/copylink.js
@@ -1,8 +1,10 @@
-﻿function myfunction() {
-    var copyText = document.getElementById("myinput")
+﻿
 
-    copyText.select();
-    copyText.setselectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-    alert("Copied link :" + copyText.value);
-}
+const copyButton = document.getElementById('btncopy');
+copyButton.addEventListener('click', async (event) => {
+    const content = document.getElementById('content').textContent;
+    await navigator.clipboard.writeText(content);
+    console.log(content);
+    const copied = await navigator.clipboard.readText();
+    console.log(copied);
+})
